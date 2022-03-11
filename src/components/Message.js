8d404ee6fda83bd492b7590a4b1a4e035e6ed56b
gatecol <tkasabuckij@gmail.com>
2022-03-11 22:16:06 +0200

    messenger

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,10 @@
 import { Box, Text } from 'native-base';
 import React from 'react';
 
-const Message = ({text, sender}) => {
-    return(
-        <Box bg="darkBlue.500" rounded="lg" m="1">
-            <Box p="1">
+const Message = ({ text, sender }) => {
+    return (
+        <Box alignItems={(sender === "crockett") ? "flex-end" : "flex-start"}>
+            <Box bg={(sender === "crockett") ? "lightBlue.200" : "lightBlue.300"} rounded="lg" m="1" p="1" w="70%">
                 <Text fontSize="md" bold>{sender}</Text>
                 <Text fontSize="sm">{text}</Text>
             </Box>
--- src/screens/MainPage.js
+++ src/screens/MainPage.js
@@ -1,88 +1,82 @@
 import { AddIcon, ArrowBackIcon, Avatar, Box, Divider, FlatList, Flex, HStack, IconButton, Input, KeyboardAvoidingView, Text, VStack } from 'native-base';
-import React from 'react';
+import React, { Component } from 'react';
 import VectorImage from 'react-native-vector-image';
 import Message from '../components/Message';
 
-const MainPage = () => {
-    const data = [{
-        text: "hello my love!",
-        sender: "crockett"
-    },
-    {
-        text: "i love you too!",
-        sender: "iro4ka_inc"
-    },
-    {
-        text: "i doing chat application by the way!",
-        sender: "crockett"
-    },
-    {
-        text: "wow, i think it's cool!",
-        sender: "iro4ka_inc"
-    },
-    {
-        text: "i doing chat application by the way!",
-        sender: "crockett"
-    },
-    {
-        text: "wow, i think it's cool!",
-        sender: "iro4ka_inc"
-    },
-    {
-        text: "i doing chat application by the way!",
-        sender: "crockett"
-    },
-    {
-        text: "wow, i think it's cool!",
-        sender: "iro4ka_inc"
-    },
-    {
-        text: "i doing chat application by the way!",
-        sender: "crockett"
-    },
-    {
-        text: "wow, i think it's cool!",
-        sender: "iro4ka_inc"
-    },
-    {
-        text: "i doing chat application by the way!",
-        sender: "crockett"
-    },
-    {
-        text: "wow, i think it's cool!",
-        sender: "iro4ka_inc"
-    }];
+export default class MainPage extends Component {
+    id = 0;
 
-    return (
-        <Box m="2" flex={1} safeArea>
-            <HStack>
-                <IconButton><ArrowBackIcon w="24px" h="24px" /></IconButton>
-                <Avatar bg="purple.100" size="md" />
-                <VStack pl="10px" pb="16px">
-                    <Text fontSize="md" bold>Crockett</Text>
-                    <Text fontSize="sm">Online</Text>
-                </VStack>
-            </HStack>
-            <Divider />
+    constructor() {
+        super();
 
-            <KeyboardAvoidingView h={{
-                base: "400px",
-                lg: "auto"
-            }} behavior={Platform.OS === "ios" ? "padding" : "height"} h="100%">
-                <VStack>
-                    <Box h="90%">
-                        <FlatList data={data} renderItem={({ item }) => <Message text={item.text} sender={item.sender} />} />
-                    </Box>
-                    <Box>
+        this.socket = undefined;
+        this.sender = "crockett";
+
+        this.state = {
+            text: 'hello from samsung',
+            data: [
+                this.createMessage('hello', 'crockett'),
+                this.createMessage('hello', 'iro4ka_inc'),
+            ],
+        }
+    }
+
+    createMessage(text, sender) {
+        return {
+            sender,
+            text,
+            id: this.id++
+        }
+    }
+
+    addMessage = (text, sender) => {
+        const newMessage = this.createMessage(text, sender);
+
+        this.setState(({data}) => {
+            const newArray = [...data, newMessage];
+            return {
+                data: newArray
+            }
+        })
+    }
+
+    componentDidMount() {
+        this.socket = new WebSocket('ws://192.168.0.102:8080');
+
+        this.socket.onopen = () => this.socket.send(this.state.text);
+
+        this.socket.onmessage = (e) => {
+            if(typeof e.data === 'string') {
+                this.addMessage(e.data, "crockett");
+            }
+        }
+    }
+
+    render() {
+        return (
+            <Box p="2" flex={1} safeArea>
+                <KeyboardAvoidingView h={{
+                    base: "400px",
+                    lg: "auto"
+                }} behavior={Platform.OS === "ios" ? "padding" : "height"} h="100%">
+                    <HStack>
+                        <IconButton><ArrowBackIcon w="24px" h="24px" /></IconButton>
+                        <Avatar bg="purple.100" size="md" />
+                        <VStack pl="10px" pb="16px">
+                            <Text fontSize="md" bold>Crockett</Text>
+                            <Text fontSize="sm">Online</Text>
+                        </VStack>
+                    </HStack>
+                    <Divider />
+                    <VStack flex="1" justifyContent="flex-end" w="100%">
+                        <FlatList data={this.state.data} renderItem={({ item }) => <Message text={item.text} sender={item.sender} />} keyExtractor={item => item.id} />
                         <HStack>
                             <IconButton><VectorImage source={require('./sticker-emoji.svg')} /></IconButton>
-                            <Input placeholder="Write a message..." variant="unstyled" />
+                            <Input placeholder="Write a message..." variant="unstyled" onSubmitEditing={(event) => { this.socket.send(event.nativeEvent.text) }} />
                         </HStack>
-                    </Box>
-                </VStack>
-            </KeyboardAvoidingView>
-        </Box>
-    );
+                    </VStack>
+                </KeyboardAvoidingView>
+            </Box>
+        )
+    };
 }
-
-export default MainPage;
